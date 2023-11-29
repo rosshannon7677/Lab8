6a488f6b62aebf5081b04f63d78ece113c5ad285
Ross Hannon <G00381859@gmit.ie>
2023-11-29 11:38:25 +0000

    L8 sobelHorizontal added

--- Lab8.py
+++ Lab8.py
@@ -1,4 +1,3 @@
-# imports
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -9,25 +8,24 @@ imgOrig = cv2.imread(r'C:\Lab8\ATU.jpg')
 # Convert the original image to grayscale
 imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
 
-# Apply Gaussian blur with different kernel sizes
-imgBlur_5x5 = cv2.GaussianBlur(imgGray, (5, 5), 0)
-imgBlur_9x9 = cv2.GaussianBlur(imgGray, (9, 9), 0)
+# Apply Sobel operator for horizontal edges (x-direction)
+sobelHorizontal = cv2.Sobel(imgGray, cv2.CV_64F, 1, 0, ksize=5)
+
+# Apply Sobel operator for vertical edges (y-direction)
+sobelVertical = cv2.Sobel(imgGray, cv2.CV_64F, 0, 1, ksize=5)
 
 # Plotting
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(15, 5))
 
 # Top Row: Original Image and Grayscale Version
-plt.subplot(2, 2, 1), plt.imshow(cv2.cvtColor(imgOrig, cv2.COLOR_BGR2RGB))
+plt.subplot(1, 3, 1), plt.imshow(cv2.cvtColor(imgOrig, cv2.COLOR_BGR2RGB))
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(2, 2, 2), plt.imshow(imgGray, cmap='gray')
+plt.subplot(1, 3, 2), plt.imshow(imgGray, cmap='gray')
 plt.title('Grayscale'), plt.xticks([]), plt.yticks([])
 
-# Bottom Row: Image Filtered with Different Kernels
-plt.subplot(2, 2, 3), plt.imshow(imgBlur_5x5, cmap='gray')
-plt.title('Gaussian Blur (5x5)'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(2, 2, 4), plt.imshow(imgBlur_9x9, cmap='gray')
-plt.title('Gaussian Blur (9x9)'), plt.xticks([]), plt.yticks([])
+# Bottom Row: Sobel Horizontal and Vertical Edges
+plt.subplot(1, 3, 3), plt.imshow(sobelHorizontal, cmap='gray')
+plt.title('Sobel Horizontal'), plt.xticks([]), plt.yticks([])
 
 plt.show()
