e52452574706d3d4c7367522ef008bb60c2f7f56
Ross Hannon <G00381859@gmit.ie>
2023-11-29 10:58:17 +0000

    L8 python file created on C drive

---
+++ Lab8.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Load the image
+#img = cv2.imread('ATU.jpeg')
+
+# Convert the image to grayscale
+#gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Plot the original and grayscale images side by side
+#plt.subplot(121), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)), plt.title('Original')
+#plt.subplot(122), plt.imshow(gray_img, cmap='gray'), plt.title('Grayscale')
+
+# Show the plot
+#plt.show()
+
+print("Hello, this is my first Python script!")
